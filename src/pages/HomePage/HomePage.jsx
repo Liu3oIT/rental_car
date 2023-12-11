79b9a7a063e3f35b3703b7c66026de4dc8bc5d89
Liu3oIT <liutyivova995@gmail.com>
2023-12-11 22:32:09 +0200

    fix header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,11 @@
+import { TiteleFavorite } from "../FavoritePage/FavoritePage.styled";
+
 const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <TiteleFavorite>
+      Wellcome, here you can find a car for temporary trips
+    </TiteleFavorite>
+  );
 };
 
 export default HomePage;
