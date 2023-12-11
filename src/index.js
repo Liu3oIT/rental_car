31d5f0415baed9d223074ef0cc4112fae684543c
Liu3oIT <liutyivova995@gmail.com>
2023-12-11 22:56:52 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-// import { PersistGate } from "redux-persist/integration/react";
+
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate > */}
       <BrowserRouter basename="/rental_car">
         <App />
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
